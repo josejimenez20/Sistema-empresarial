74ceb470887eb5b5e4f2186df2fa72a0ce559dd9
Jose Jimenez <josejimenez2003w@gmail.com>
2026-02-13 21:13:52 -0600

    feat: BASE DE DATOS REPARADA

--- routes/web.php
+++ routes/web.php
@@ -11,3 +11,8 @@ Route::get('/contabilidad', [AccountingController::class, 'index'])->name('accou
 Route::post('/contabilidad/apertura', [AccountingController::class, 'storeOpening'])->name('accounting.opening');
 Route::post('/contabilidad/transaccion', [AccountingController::class, 'storeTransaction'])->name('accounting.transaction');
 Route::delete('/contabilidad/transaccion/{id}', [AccountingController::class, 'destroy'])->name('accounting.destroy');
+// RUTA TEMPORAL PARA REPARAR LA BASE DE DATOS EN PRODUCTION
+Route::get('/reparar-db', function () {
+    Artisan::call('migrate:fresh', ['--seed' => true, '--force' => true]);
+    return "¡Base de datos reparada y catálogo cargado con éxito!";
+});
